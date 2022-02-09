ff0b2f2229a144a724898f7b7d97d6be1d909ce8
edewajosh <joshedewa@gmail.com>
2022-02-09 16:45:55 +0300

    added requirements.txt

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uzorcwtz)65u)(^!ziia%h%9ad^1m2bt$v17p2d9ayc%8ob3ct'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
